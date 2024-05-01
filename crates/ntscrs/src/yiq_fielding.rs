271c40bad4a1508c0315410d443dcad52d5e59e7
I'm Luciano <lucianothewindowsfanisback@gmail.com>
2024-05-01 14:57:59 -0400

    Update yiq_fielding.rs

--- crates/ntscrs/src/yiq_fielding.rs
+++ crates/ntscrs/src/yiq_fielding.rs
@@ -6,7 +6,7 @@ use rayon::prelude::*;
 
 #[inline(always)]
 pub fn rgb_to_yiq([r, g, b]: [f32; 3]) -> [f32; 3] {
-    if self.color_space = 0.0 {
+    if &PALmode.color_space = 0.0 {
     const YIQ_MATRIX: Mat3A = Mat3A::from_cols(
         Vec3A::new(0.299, -0.14713, 0.615),
         Vec3A::new(0.587, -0.28886, -0.51499),
@@ -26,7 +26,7 @@ pub fn rgb_to_yiq([r, g, b]: [f32; 3]) -> [f32; 3] {
 
 #[inline(always)]
 pub fn yiq_to_rgb([y, i, q]: [f32; 3]) -> [f32; 3] {
-    if self.color_space = 0.0 {
+    if &PALmode.color_space = 0.0 {
     const RGB_MATRIX: Mat3A = Mat3A::from_cols(
         Vec3A::new(1.0, 1.0, 1.0),
         Vec3A::new(0.0, -0.39465, 2.03211),
