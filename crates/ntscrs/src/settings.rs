74474e598710ba3a6d4e808923fdbae7e9fbece5
I'm Luciano <lucianothewindowsfanisback@gmail.com>
2024-05-01 18:22:43 -0400

    Update settings.rs

--- crates/ntscrs/src/settings.rs
+++ crates/ntscrs/src/settings.rs
@@ -629,7 +629,7 @@ macro_rules! impl_get_field_ref {
                 .$borrow_op(),
 
             SettingID::BANDWIDTH_SCALE => $settings.bandwidth_scale.$borrow_op(),
-            SettingID::PAL_MODE => $settings.pal_mode.$borrow_op(),
+            SettingID::PAL_MODE => $settings.pal_mode.chroma_subcarrier.color_space.v_bandwidth.$borrow_op(),
             SettingID::RANDOM_SEED => $settings.random_seed.$borrow_op(),
 
             SettingID::CHROMA_PHASE_ERROR => $settings.chroma_phase_error.$borrow_op(),
