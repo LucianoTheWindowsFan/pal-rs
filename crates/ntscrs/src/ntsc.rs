4ec9cc5c77b86a3cc190407c7742705afa2117cd
I'm Luciano <lucianothewindowsfanisback@gmail.com>
2024-05-01 15:33:18 -0400

    Update ntsc.rs

--- crates/ntscrs/src/ntsc.rs
+++ crates/ntscrs/src/ntsc.rs
@@ -8,6 +8,8 @@ use rand_xoshiro::Xoshiro256PlusPlus;
 use rayon::prelude::*;
 use simdnoise::{NoiseBuilder, Settings, SimplexSettings};
 
+mod settings;
+
 use crate::{
     filter::TransferFunction,
     random::{Geometric, Seeder},
@@ -22,14 +24,12 @@ struct CommonInfo {
     seed: u64,
     frame_num: usize,
     bandwidth_scale: f32,
-    chroma_subcarrier: f32,
-    v_bandwidth: f32,
 }
 
 // 315/88 Mhz rate * 4
 // TODO: why do we multiply by 4? composite-video-simulator does this for every filter and ntscqt defines NTSC_RATE the
 // same way as we do here.
-const NTSC_RATE: f32 = (info.chroma_subcarrier / 88.0) * 4.0;
+const NTSC_RATE: f32 = (chroma_subcarrier / 88.0) * 4.0;
 
 /// Create a simple constant-k lowpass filter with the given frequency cutoff, which can then be used to filter a signal.
 pub fn make_lowpass(cutoff: f32, rate: f32) -> TransferFunction {
@@ -997,8 +997,6 @@ impl NtscEffect {
             seed,
             frame_num,
             bandwidth_scale: self.bandwidth_scale,
-            chroma_subcarrier,
-            v_bandwidth,
         };
 
         let mut scratch_buffer = ScratchBuffer::new(yiq.y.len());
