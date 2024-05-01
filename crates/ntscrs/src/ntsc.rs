bbc2a8e1211e7892c0cd3254aee34273be746d4c
I'm Luciano <lucianothewindowsfanisback@gmail.com>
2024-05-01 17:28:15 -0400

    Update ntsc.rs

--- crates/ntscrs/src/ntsc.rs
+++ crates/ntscrs/src/ntsc.rs
@@ -20,7 +20,7 @@ pub use crate::settings::*;
 // 315/88 Mhz rate * 4
 // TODO: why do we multiply by 4? composite-video-simulator does this for every filter and ntscqt defines NTSC_RATE the
 // same way as we do here.
-const NTSC_RATE: f32 = (315000000.00 / 88.0) * 4.0;
+const NTSC_RATE: f32 = (390158450.00 / 88.0) * 4.0;
 
 /// Create a simple constant-k lowpass filter with the given frequency cutoff, which can then be used to filter a signal.
 pub fn make_lowpass(cutoff: f32, rate: f32) -> TransferFunction {
@@ -1020,7 +1020,7 @@ impl NtscEffect {
 
         if self.composite_preemphasis > 0.0 {
             let preemphasis_filter = make_lowpass(
-                (315000000.0 / 88.0 / 2.0) * self.bandwidth_scale,
+                (390158450.0 / 88.0 / 2.0) * self.bandwidth_scale,
                 NTSC_RATE * self.bandwidth_scale,
             );
             filter_plane(
