eb3b18accd7d9b90d99626ca7b16d697c29a4601
I'm Luciano <lucianothewindowsfanisback@gmail.com>
2024-05-01 20:38:28 -0400

    Update ntsc.rs

--- crates/ntscrs/src/ntsc.rs
+++ crates/ntscrs/src/ntsc.rs
@@ -13,7 +13,7 @@ use crate::{
     random::{Geometric, Seeder},
     shift::{shift_row, shift_row_to, BoundaryHandling},
     yiq_fielding::{YiqField, YiqOwned, YiqView},
-    settings.PALparams::{chroma_subcarrier, v_bandwidth},
+    settings::{PALparams.chroma_subcarrier, PALparams.v_bandwidth},
 };
 
 pub use crate::settings::*;
@@ -28,7 +28,7 @@ struct CommonInfo {
 // 315/88 Mhz rate * 4
 // TODO: why do we multiply by 4? composite-video-simulator does this for every filter and ntscqt defines NTSC_RATE the
 // same way as we do here.
-const NTSC_RATE: f32 = (chroma_subcarrier / 88.0) * 4.0;
+const NTSC_RATE: f32 = (PALparams.chroma_subcarrier / 88.0) * 4.0;
 
 /// Create a simple constant-k lowpass filter with the given frequency cutoff, which can then be used to filter a signal.
 pub fn make_lowpass(cutoff: f32, rate: f32) -> TransferFunction {
@@ -219,7 +219,7 @@ fn luma_filter(frame: &mut YiqView, filter_mode: LumaLowpass) {
 /// that statement seems unsourced and I can't find any info on it...
 fn composite_chroma_lowpass(frame: &mut YiqView, info: &CommonInfo, filter_type: FilterType) {
     let i_filter = make_lowpass_for_type(1300000.0, NTSC_RATE * info.bandwidth_scale, filter_type);
-    let q_filter = make_lowpass_for_type(v_bandwidth, NTSC_RATE * info.bandwidth_scale, filter_type);
+    let q_filter = make_lowpass_for_type(PALparams.v_bandwidth, NTSC_RATE * info.bandwidth_scale, filter_type);
 
     let width = frame.dimensions.0;
 
@@ -1021,7 +1021,7 @@ impl NtscEffect {
 
         if self.composite_preemphasis > 0.0 {
             let preemphasis_filter = make_lowpass(
-                (chroma_subcarrier / 88.0 / 2.0) * self.bandwidth_scale,
+                (PALparams.chroma_subcarrier / 88.0 / 2.0) * self.bandwidth_scale,
                 NTSC_RATE * self.bandwidth_scale,
             );
             filter_plane(
