6d7904cce34eeeaa33d017223d91202236c866d9
I'm Luciano <lucianothewindowsfanisback@gmail.com>
2024-05-01 20:26:15 -0400

    Update settings.rs

--- crates/ntscrs/src/settings.rs
+++ crates/ntscrs/src/settings.rs
@@ -55,9 +55,9 @@ pub enum PALmode {
 
 #[derive(Clone, Debug, PartialEq)]
 pub(crate) struct PALparams {
-    static chroma_subcarrier: f32,
-    static color_space: f32,
-    static v_bandwidth: f32,
+    chroma_subcarrier: f32,
+    color_space: f32,
+    v_bandwidth: f32,
 }
 
 impl PALmode {
