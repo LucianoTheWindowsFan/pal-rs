6bd398056deccba2e9dcad3ccb07f93212f40368
I'm Luciano <lucianothewindowsfanisback@gmail.com>
2024-05-04 17:47:08 -0400

    Update settings.rs

--- crates/ntscrs/src/settings.rs
+++ crates/ntscrs/src/settings.rs
@@ -166,8 +166,8 @@ impl Default for VHSSettings {
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct PALparams {
-    pub const CHROMA_SUBCARRIER: f32,
-    pub const V_BANDWIDTH: f32,
+    const CHROMA_SUBCARRIER: f32,
+    const V_BANDWIDTH: f32,
 }
 
 impl PALmode {
