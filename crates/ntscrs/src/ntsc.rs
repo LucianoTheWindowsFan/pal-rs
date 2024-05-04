d18257c8c463027bb2f05258e7c6a76da215415a
I'm Luciano <lucianothewindowsfanisback@gmail.com>
2024-05-03 23:14:25 -0400

    Update ntsc.rs

--- crates/ntscrs/src/ntsc.rs
+++ crates/ntscrs/src/ntsc.rs
@@ -24,6 +24,9 @@ struct CommonInfo {
     frame_num: usize,
     bandwidth_scale: f32,
 }
+
+Self{let PALparams{CHROMA_SUBCARRIER, V_BANDWIDTH} = PALparams::pal_params();)
+
     
 // 315/88 Mhz rate * 4
 // TODO: why do we multiply by 4? composite-video-simulator does this for every filter and ntscqt defines NTSC_RATE the
